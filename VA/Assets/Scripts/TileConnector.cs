cade8f45dcb99e6783d66d65c978ec477c644d00
Yanis Guaye <yanis.guaye@epitech.eu>
2024-06-04 16:02:58 +0200

    feat: add collision-based tile connection logic

--- VA/Assets/Scripts/TileConnector.cs
+++ VA/Assets/Scripts/TileConnector.cs
@@ -1,18 +1,45 @@
+using OrdureX.AR;
 using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Tilemaps;
 
 namespace OrdureX
 {
     [RequireComponent(typeof(Collider))]
     public class TileConnector : MonoBehaviour
     {
+        public SurfaceTile OwnTile;
+
+        public SurfaceTile ConnectedTile
+        {
+            get
+            {
+                if (m_ConnectedTo == null)
+                {
+                    return null;
+                }
+                return m_ConnectedTo.OwnTile;
+            }
+        }
+
+        private TileConnector m_ConnectedTo;
+
         private void OnTriggerEnter(Collider other)
         {
-            Debug.Log($"Collision started: {transform.position} -> {other.transform.position}");
+            if (other.TryGetComponent<TileConnector>(out var otherConnector))
+            {
+                m_ConnectedTo = otherConnector;
+                Debug.Log($"Tile Connection: {transform.position} -> {other.transform.position}");
+            }
         }
 
         private void OnTriggerExit(Collider other)
         {
-            Debug.Log($"Collision ended: {transform.position} -> {other.transform.position}");
+            if (other.TryGetComponent<TileConnector>(out var otherConnector) && otherConnector == m_ConnectedTo)
+            {
+                m_ConnectedTo = null;
+                Debug.Log($"Tile Disconnection: {transform.position} -> {other.transform.position}");
+            }
         }
     }
 }
