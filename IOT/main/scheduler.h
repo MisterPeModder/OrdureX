cc006a18f0ef4d1b750e09597b2db9a717387aea
MelvinCou <113338357+MelvinCou@users.noreply.github.com>
2024-06-14 14:32:42 +0000

    feat(iot): add a task scheduler

---
+++ IOT/main/scheduler.h
@@ -0,0 +1,28 @@
+#ifndef SCHEDULER_H
+#define SCHEDULER_H
+
+#define MAX_TASKS 20
+
+struct task_t {
+  // Delay before the task is executed (real execution delay may vary)
+  unsigned long delay;
+  // Time when the task was last executed
+  unsigned long lastRun;
+  // Function must be called indefinitely
+  bool loop;
+  // Function to be executed
+  void (*task)();
+};
+
+class scheduler {
+public:
+  scheduler();
+  ~scheduler();
+
+  bool addTask(task_t task);
+  void run();
+private:
+  task_t tasks[MAX_TASKS];
+};
+
+#endif  // SCHEDULER_H
