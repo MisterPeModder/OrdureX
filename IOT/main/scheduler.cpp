af5602ee88423bc7e2a0b612b3e666e67e702c30
MelvinCou <melvin.courjaud@epitech.eu>
2024-06-17 13:38:34 +0200

    refactor(iot): refactor scheduler class names to follow naming conventions

--- IOT/main/scheduler.cpp
+++ IOT/main/scheduler.cpp
@@ -8,7 +8,7 @@
 #define GET_TIME (unsigned long)std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count()
 #endif
 
-scheduler::scheduler() {
+Scheduler::Scheduler() {
   for (int i = 0; i < MAX_TASKS; i++) {
     tasks[i].delay = 0;
     tasks[i].lastRun = 0;
@@ -17,10 +17,10 @@ scheduler::scheduler() {
   }
 }
 
-scheduler::~scheduler() {
+Scheduler::~Scheduler() {
 }
 
-bool scheduler::addTask(task_t task) {
+bool Scheduler::addTask(task_t task) {
   for (int i = 0; i < MAX_TASKS; i++) {
     if (tasks[i].task == nullptr) {
       tasks[i] = task;
@@ -30,14 +30,14 @@ bool scheduler::addTask(task_t task) {
   return false;
 }
 
-void scheduler::run() {
+void Scheduler::run() {
   for (int i = 0; i < MAX_TASKS; i++) {
     if (tasks[i].task != nullptr && GET_TIME >= tasks[i].lastRun + tasks[i].delay) {
       tasks[i].task(tasks[i].context);
-      if (!tasks[i].loop) {
-        tasks[i].task = nullptr;
-      } else {
+      if (tasks[i].loop) {
         tasks[i].lastRun = GET_TIME;
+      } else {
+        tasks[i].task = nullptr;
       }
     }
   }
