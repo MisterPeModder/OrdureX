5e95a576d138b08ca3d645644c01529f0e7d8367
Yanis Guaye <yanis.guaye@epitech.eu>
2024-07-07 23:10:29 +0200

    feat(ar): add truck path visualization

--- VA/Assets/Scripts/GarbageTruckAI.cs
+++ VA/Assets/Scripts/GarbageTruckAI.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using System.Collections;
 using UnityEngine;
 using UnityEngine.AI;
+using Unity.VisualScripting;
 
 namespace OrdureX
 {
@@ -15,6 +16,24 @@ namespace OrdureX
         [Tooltip("The distance at which the truck is considered close enough to a trash can.")]
         [SerializeField] private float m_TrashCollectDistance = 1.0f;
         [SerializeField] private Transform m_CurrentTarget;
+        [SerializeField] private GameObject m_PathCornerPrefab;
+        [SerializeField] private ScaledProjection m_PathCornerProjectionPrefab;
+
+
+        [SerializeField]
+        private Transform m_BigOrigin;
+        [SerializeField]
+        private Transform m_SmallOrigin;
+        [SerializeField]
+        private LineRenderer m_PathLine;
+
+        private SettingsManager m_SettingsManager;
+
+        private const int k_MaxCorners = 16;
+        private GameObject m_PathCornesParent;
+        private readonly Vector3[] m_Corners = new Vector3[k_MaxCorners];
+        private readonly GameObject[] m_PathCorners = new GameObject[k_MaxCorners];
+        private readonly ScaledProjection[] m_PathCornerProjections = new ScaledProjection[k_MaxCorners];
 
         public List<Transform> Targets
         {
@@ -22,14 +41,82 @@ namespace OrdureX
             set => m_Targets = value;
         }
 
-        void Awake()
+        private void Awake()
         {
             m_Agent = GetComponent<NavMeshAgent>();
+            m_SettingsManager = FindObjectOfType<SettingsManager>();
         }
 
-        void Start()
+        public void Initialize(Transform bigOrigin, Transform smallOrigin)
         {
+            m_BigOrigin = bigOrigin;
+            m_SmallOrigin = smallOrigin;
+        }
+
+        private void Start()
+        {
+            m_PathCornesParent = new GameObject("PathCorners");
+            for (int i = 0; i < k_MaxCorners; i++)
+            {
+                var corner = Instantiate(m_PathCornerPrefab, Vector3.zero, Quaternion.identity);
+                corner.transform.SetParent(m_PathCornesParent.transform);
+                corner.name = $"Path Corner #{i}";
+                corner.SetActive(false);
+                m_PathCorners[i] = corner;
+
+                var projection = Instantiate(m_PathCornerProjectionPrefab, Vector3.zero, Quaternion.identity);
+                projection.Initialize(m_BigOrigin, m_SmallOrigin, corner.transform);
+                projection.transform.SetParent(m_PathCornesParent.transform);
+                projection.name = $"Path Corner Projection #{i}";
+                projection.gameObject.SetActive(false);
+                m_PathCornerProjections[i] = projection;
+            }
             StartCoroutine(TruckRoutine());
+        }
+
+        private void OnDestroy()
+        {
+            foreach (var corner in m_PathCorners)
+            {
+                Destroy(corner);
+            }
+            Destroy(m_PathCornesParent);
+        }
+
+        private void Update()
+        {
+            if (!m_SettingsManager.ShowTruckPath || !m_Agent.hasPath)
+            {
+                for (int i = 0; i < k_MaxCorners; i++)
+                {
+                    m_PathCorners[i].SetActive(false);
+                    m_PathCornerProjections[i].gameObject.SetActive(false);
+                }
+                return;
+            }
+            int nCorners = m_Agent.path.GetCornersNonAlloc(m_Corners);
+
+            var posOffset = Vector3.up * 0.2f;
+            for (int i = 0; i < k_MaxCorners; i++)
+            {
+                if (i < nCorners)
+                {
+                    m_PathCorners[i].transform.position = m_Corners[i] + posOffset;
+                    m_PathCorners[i].SetActive(true);
+                    m_PathCornerProjections[i].gameObject.SetActive(true);
+                }
+                else
+                {
+                    m_PathCorners[i].SetActive(false);
+                    m_PathCornerProjections[i].gameObject.SetActive(false);
+                }
+            }
+
+            m_PathLine.positionCount = nCorners;
+            for (int i = 0; i < nCorners; i++)
+            {
+                m_PathLine.SetPosition(i, m_PathCornerProjections[i].transform.position);
+            }
         }
 
         private IEnumerator TruckRoutine()
